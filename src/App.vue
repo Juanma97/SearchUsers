874fe9ba0609fca5b226238b513f452a73125134
Juan Manuel Perez Toro <juanma97@outlook.com>
2018-11-09 09:50:37 +0000

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SeekerHeader/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import SeekerHeader from './components/SeekerHeader.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
+    SeekerHeader,
   },
 };
 </script>
@@ -24,5 +24,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display:flex;
+  
+}
+
+#app img{
+  width: 50px;
+  height: 50px;
 }
 </style>
---
+++ src/components/SeekerHeader.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="seeker">
+    <input type="search" v-model='query'>
+    <button type="button">Search user</button>
+    <p>Searching for: {{ query }}</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'SeekerHeade',
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      query: 'Hola',
+      users: null,
+    };
+  },
+  async created() {
+    await Vue.axios.get(api)
+      .then((response) => {
+        
+      });
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.seeker{
+  display: flex;
+}
+
+.seeker button{
+  width: 100px;
+  height: 40px;
+  padding: 10px;
+  background: lightblue;
+  border-radius: 10px;
+  margin: 16px;
+}
+
+.seeker input[type="search"]{
+  width: 300px;
+  height: 40px;
+  padding: 10px;
+  border-radius: 10px;
+  margin: 16px;
+}
+
+.seeker p{
+  display: flex;
+  align-items: center;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
